5a76022fd7dd1ca3725e63a7779e1baaeecce648
AndGate92 <mrandre.epfl@gmail.com>
2019-09-16 20:52:04 +0100

    Add outer window

--- top/src/main.cpp
+++ top/src/main.cpp
@@ -1,11 +1,18 @@
 #include <qt5/QtWidgets/QApplication>
 #include <qt5/QtWidgets/QLabel>
 #include <qt5/QtWidgets/QWidget>
+#include <qt5/QtCore/QSize>
 
 int main (int argc, char* argv[]) {
 	QApplication app(argc, argv);
-	QLabel hello("<center>Welcome to Qt</center>");
-	hello.setWindowTitle("Test");
+	QSize winSize(320,400);
+	QWidget window;
+	window.setWindowTitle("Test");
+	window.resize(winSize);
+	window.show();
+	QLabel hello("Welcome to Qt", &window);
+	hello.setAlignment(Qt::AlignCenter);
+	hello.resize(winSize);
 	hello.show();
 	return app.exec();
 }
