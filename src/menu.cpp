9b40fedd5452af1bc2789aad7ad994ac36d72849
AndGate92 <mrandre.epfl@gmail.com>
2019-09-29 11:48:12 +0100

    Expand menu only if menu bar is visible

--- src/menu.cpp
+++ src/menu.cpp
@@ -42,7 +42,10 @@ void menu::Menu::createShortcuts() {
 }
 
 void menu::Menu::expandSlot() {
-	QINFO_PRINT(global_types::qinfo_level_e::ZERO, menuOverall, "Expand menu " << this->menuName << " because shortcut key " << this->key.toString() << " has been pressed");
 
-	this->winMenu->show();
+	// menu is expanded only if menu bar is visible
+	if (this->menuBar->isVisible()) {
+		QINFO_PRINT(global_types::qinfo_level_e::ZERO, menuOverall, "Expand menu " << this->menuName << " because shortcut key " << this->key.toString() << " has been pressed");
+		this->winMenu->show();
+	}
 }
