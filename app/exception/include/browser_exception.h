ef76291b012b1cc13f1a48c659a90723f3ada8e9
Andrea Gianarda <mrandre.epfl@gmail.com>
2020-02-16 16:05:53 +0000

    Add browser exception class (Derived class of QException)

---
+++ app/exception/include/browser_exception.h
@@ -0,0 +1,79 @@
+#ifndef BROWSER_EXCEPTION_H
+#define BROWSER_EXCEPTION_H
+/**
+ * @copyright
+ * @file browser_exception.h
+ * @author Andrea Gianarda
+ * @date 16th Febraury 2020
+ * @brief Browser exception functions
+ */
+
+#include <qt5/QtCore/QLoggingCategory>
+#include <qt5/QtCore/QException>
+
+/** @defgroup BrowserExceptionGroup Browser Exception Doxygen Group
+ *  Browser Exception functions and classes
+ *  @{
+ */
+
+Q_DECLARE_LOGGING_CATEGORY(browserExceptionOverall)
+Q_DECLARE_LOGGING_CATEGORY(browserExceptionPrint)
+
+namespace browser_exception {
+
+	/**
+	 * @brief BrowserException class
+	 *
+	 */
+	class BrowserException : public QException {
+
+		public:
+
+			/**
+			 * @brief Function: explicit BrowserException(const QString & msg)
+			 *
+			 * \param msg: message of the exception
+			 *
+			 * Browser exception constructor
+			 */
+			explicit BrowserException(const QString & msg);
+
+			/**
+			 * @brief Function: ~BrowserException()
+			 *
+			 * Browser exception destructor
+			 */
+			~BrowserException();
+
+			/**
+			 * @brief Function: void raise() const override
+			 *
+			 * Raise exception
+			 */
+			void raise() const override;
+
+			/**
+			 * @brief Function: browser_exception::BrowserException * clone() const override
+			 *
+			 * \return a deep copy of the exception
+			 *
+			 * Clone exception
+			 */
+			browser_exception::BrowserException * clone() const override;
+
+			/**
+			 * @brief Function: QString print() const
+			 *
+			 * Print message
+			 */
+			QString print() const;
+
+		private:
+			QString message;
+
+	};
+
+}
+/** @} */ // End of BrowserExceptionGroup group
+
+#endif // BROWSER_EXCEPTION_H
