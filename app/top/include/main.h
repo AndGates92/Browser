0805b84fd049013cb5997a1ba5dee6fec9e0d75e
Andrea Gianarda <mrandre.epfl@gmail.com>
2020-02-17 22:55:45 +0000

    Replace all QCRITICAL_PRINT with exceptions

---
+++ app/top/include/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * @copyright
+ * @file main.h
+ * @author Andrea Gianarda
+ * @date 17th February 2020
+ * @brief Main program header file
+*/
+
+
+#include <qt5/QtCore/QLoggingCategory>
+
+/** @defgroup MainGroup Main Doxygen Group
+ *  Main functions and classes
+ *  @{
+ */
+
+Q_DECLARE_LOGGING_CATEGORY(mainBrowserException)
+
+/** @} */ // End of MainGroup group
+
+#endif // MAIN_H
