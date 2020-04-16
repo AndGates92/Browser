57af25d5ae1459768a5b207e8959ffb5b1536746
Andrea Gianarda <mrandre.epfl@gmail.com>
2020-04-16 20:19:39 +0200

    Add global classes to define a set of classes to be used throughout the project

---
+++ app/common/include/global_classes.h
@@ -0,0 +1,42 @@
+#ifndef GLOBAL_CLASSES_H
+#define GLOBAL_CLASSES_H
+/**
+ * @copyright
+ * @file global_classes.h
+ * @author Andrea Gianarda
+ * @date 19th of February 2020
+ * @brief Global Classes header file
+*/
+
+#include <string>
+
+/** @defgroup GlobalClassesGroup Global Classes Doxygen Group
+ *  Global Classes
+ *  @{
+ */
+
+namespace global_classes {
+
+	/**
+	 * @brief StringCompare struct
+	 *
+	 */
+	struct StringCompare {
+		// Struct sets the default access to public
+		/**
+		 * @brief Function: bool operator() (const std::string & rhs, const std::string & lhs)
+		 *
+		 * \param rhs: string to compare
+		 * \param lhs: string to compare
+		 *
+		 * \return: true if the two strings are identical, false otherwise
+		 *
+		 * This function compares two std::string objects
+		 */
+		bool operator() (const std::string & rhs, const std::string & lhs);
+	};
+
+}
+/** @} */ // End of GlobalClassesGroup group
+
+#endif // GLOBAL_CLASSES_H
---
+++ app/common/src/global_classes.cpp
@@ -0,0 +1,13 @@
+/**
+ * @copyright
+ * @file global_classes.cpp
+ * @author Andrea Gianarda
+ * @date 31st of Mqrch 2020
+ * @brief Global class definition
+ */
+
+#include "global_classes.h"
+
+bool global_classes::StringCompare::operator() (const std::string & rhs, const std::string & lhs) {
+		return (rhs.compare(lhs) < 0);
+}
